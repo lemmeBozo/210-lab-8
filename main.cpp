16bfd6bfa5bd4eda662c3108a15ebc289c66c4a0
Erick_Pascual_Bautista <erickpascualbautista@gmail.com>
2024-09-23 22:17:14 -0700

    Added message

--- main.cpp
+++ main.cpp
@@ -5,7 +5,9 @@ using namespace std;
 // COMSC-210 | Lab XXXX: XXXXXXXXX | Erick Pascual-Bautista
 
 int main() {
-
+    cout << "Hey this is my first time using git via terminal" << endl;
+    cout << "Im used to using github and uploading my files manually" << endl;
+    cout << "So far this has been a very enlightening time" << endl;
     return 0;
 }
 
